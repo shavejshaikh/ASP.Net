eb6f9900762cc7d5488093d734fd029119c1d127
shavejshaikh <shavejshaikh000000@gmail.com>
2021-02-28 17:18:52 +0530

    First Commit

---
+++ FirstApp/Pages/BookList/index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApp.Pages.BookList
+{
+    public class indexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public indexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public void OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
